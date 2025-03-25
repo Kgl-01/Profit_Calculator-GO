3ea10f0436a4ca14018270da1b1ae84716f3b22f
Karthik Gowda L <106536496+Kgl-01@users.noreply.github.com>
2025-03-25 10:05:29 +0000

    alternative return syntax for a GO function

--- profit_calculator.go
+++ profit_calculator.go
@@ -16,7 +16,7 @@ func main() {
 	outputText("Tax Rate: ")
 	fmt.Scan(&taxRate)
 
-	ebt, profit, ratio := calculateEbtProfitAndRatio(revenue, expenses, taxRate)
+	ebt, profit, ratio := calculateEbtProfitAndRatioAlt(revenue, expenses, taxRate)
 
 	// ebt := revenue - expenses
 	// profit := ebt * (1 - taxRate/100)
@@ -40,6 +40,14 @@ func calculateEbtProfitAndRatio(revenue, expenses, taxRate float64) (float64, fl
 	ebt := revenue - expenses
 	profit := ebt * (1 - taxRate/100)
 	ratio := ebt / profit
-
 	return ebt, profit, ratio
+}
+
+/*Alternative return snyntax for function*/
+func calculateEbtProfitAndRatioAlt(revenue, expenses, taxRate float64) (ebt, profit, ratio float64) {
+	ebt = revenue - expenses
+	profit = ebt * (1 - taxRate/100)
+	ratio = ebt / profit
+	//return ebt, profit, ratio (or)
+	return
 }
